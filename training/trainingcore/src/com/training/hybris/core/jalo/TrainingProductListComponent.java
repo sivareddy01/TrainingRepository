052e5ed910b537ed9304f6db844b5e109ca70585
raja <rajareddyg0506@gmail.com>
2018-07-13 20:37:22 +0530

    JIRA:EC 1188 adding config folder

--- training/trainingcore/src/com/training/hybris/core/jalo/TrainingProductListComponent.java
+++ training/trainingcore/src/com/training/hybris/core/jalo/TrainingProductListComponent.java
@@ -1,25 +1,57 @@
 package com.training.hybris.core.jalo;
 
+import de.hybris.platform.cms2lib.components.GeneratedProductListComponent;
 import de.hybris.platform.jalo.Item;
 import de.hybris.platform.jalo.JaloBusinessException;
 import de.hybris.platform.jalo.SessionContext;
 import de.hybris.platform.jalo.type.ComposedType;
+
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
-public class TrainingProductListComponent extends GeneratedTrainingProductListComponent
+
+public class TrainingProductListComponent extends GeneratedProductListComponent
 {
 	@SuppressWarnings("unused")
-	private final static Logger LOG = Logger.getLogger( TrainingProductListComponent.class.getName() );
-	
+	private final static Logger LOG = Logger.getLogger(TrainingProductListComponent.class.getName());
+
 	@Override
-	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
+	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes)
+			throws JaloBusinessException
 	{
 		// business code placed here will be executed before the item is created
 		// then create the item
-		final Item item = super.createItem( ctx, type, allAttributes );
+		final Item item = super.createItem(ctx, type, allAttributes);
 		// business code placed here will be executed after the item was created
 		// and return the item
 		return item;
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.hybris.platform.cms2lib.components.GeneratedProductListComponent#getCategoryCode(de.hybris.platform.jalo.
+	 * SessionContext)
+	 */
+	@Override
+	public String getCategoryCode(final SessionContext arg0)
+	{
+		// YTODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.hybris.platform.cms2lib.components.GeneratedProductListComponent#getProductCodes(de.hybris.platform.jalo.
+	 * SessionContext)
+	 */
+	@Override
+	public List<String> getProductCodes(final SessionContext arg0)
+	{
+		// YTODO Auto-generated method stub
+		return null;
+	}
+
 }
--- training/trainingstorefront/web/webroot/WEB-INF/views/responsive/cms/trainingproductlistcomponent.jsp
+++ training/trainingstorefront/web/webroot/WEB-INF/views/responsive/cms/trainingproductlistcomponent.jsp
@@ -2,9 +2,7 @@
 	pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="cms" uri="http://hybris.com/tld/cmstags"%>
-hiii
-
-${component.products}
+hiii<br>
 <c:forEach items="${component.products}" var="product">
 ${product.name}<br>
 ${product.picture}<br>
