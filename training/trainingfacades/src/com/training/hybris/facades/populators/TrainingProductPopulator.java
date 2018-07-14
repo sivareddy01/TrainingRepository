45e40e034cd6302f68d51c4d9fdb56533b2434b7
raja <rajareddyg0506@gmail.com>
2018-07-14 20:04:03 +0530

    Jira:Ec siva Images added to  homepage

--- training/trainingfacades/src/com/training/hybris/facades/populators/TrainingProductPopulator.java
+++ training/trainingfacades/src/com/training/hybris/facades/populators/TrainingProductPopulator.java
@@ -3,11 +3,14 @@
  */
 package com.training.hybris.facades.populators;
 
+import de.hybris.platform.commercefacades.product.data.ImageData;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.model.media.MediaModel;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.enumeration.EnumerationService;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
 
 import javax.annotation.Resource;
 
@@ -23,6 +26,12 @@ public class TrainingProductPopulator implements Populator<ProductModel, Product
 
 	@Resource(name = "enumerationService")
 	private EnumerationService enumerationService;
+
+	@Resource(name = "imagePopulator")
+	private Populator imagePopulator;
+
+	@Resource(name = "imageConverter")
+	private Converter<MediaModel, ImageData> imageConverter;
 
 	/*
 	 * (non-Javadoc)
@@ -45,6 +54,18 @@ public class TrainingProductPopulator implements Populator<ProductModel, Product
 			final String test = ((MyProductModel) source).getTest();
 			target.setTest(test);
 		}
+
+		if (null != source.getPicture())
+		{
+			final MediaModel pictureModel = source.getPicture();
+			//			final ImageData imagedata = new ImageData();
+			//			imagePopulator.populate(pictureModel, imagedata);
+			//			target.setPicture(imagedata);
+
+			final ImageData imagedata = imageConverter.convert(pictureModel);
+			target.setPicture(imagedata);
+		}
+
 	}
 
 }
